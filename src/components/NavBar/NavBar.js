c33c345d9780c37d8c97aff9af41b453d2c467c8
Joey Zelinka <cheflife365@gmail.com>
2021-09-30 12:10:02 -0400

    homepage coming along pt2

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "bootswatch/dist/cyborg/bootstrap.min.css";
 import { motion } from 'framer-motion';
+import { Button } from 'react-bootstrap'
 export default function NavBar() {
     const variants = {
         hidden: {
@@ -29,23 +30,23 @@ export default function NavBar() {
                     <div class="collapse navbar-collapse" id="navbarColor02">
                         <ul class="navbar-nav me-auto">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">WoR Weekly Gaming Blog</a>
+                                <Button variant="link" class="nav-link" href="#">WoR Weekly Gaming Blog</Button>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Contact</a>
+                                <Button variant="link" class="nav-link" href="#">Contact</Button>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
+                                <Button variant="link" class="nav-link" href="#">About</Button>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                                    aria-haspopup="true" aria-expanded="false">Realms</a>
+                                <Button variant="link" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                                    aria-haspopup="true" aria-expanded="false">Realms</Button>
                                 <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="#">Magic: the Gathering</a>
-                                    <a class="dropdown-item" href="#">Role Playing Games</a>
-                                    <a class="dropdown-item" href="#">Warhammer 40,000</a>
-                                    <a class="dropdown-item" href="#">Flesh and Blood TCG</a>
-                                    <a class="dropdown-item" href="#">Pokemon Trading Card Game</a>
+                                    <Button variant="link" class="dropdown-item" href="#">Magic: the Gathering</Button>
+                                    <Button variant="link" class="dropdown-item" href="#">Role Playing Games</Button>
+                                    <Button variant="link" class="dropdown-item" href="#">Warhammer 40,000</Button>
+                                    <Button variant="link" class="dropdown-item" href="#">Flesh and Blood TCG</Button>
+                                    <Button variant="link" class="dropdown-item" href="#">Pokemon Trading Card Game</Button>
                                 </div>
                             </li>
                         </ul>
