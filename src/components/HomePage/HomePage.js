146445565cfd7b1ee2b1cab0551cb0bcda42d9fc
Joey Zelinka <cheflife365@gmail.com>
2021-10-04 13:20:10 -0400

    mtg home set

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Realms from './Realms/Realms.js'
+import "bootswatch/dist/cyborg/bootstrap.min.css";
+import { Badge } from 'react-bootstrap'
+
 
 export default function HomePage() {
     return (
         <div>
-            
+            <h1 className="head">
+            <Badge bg="primary">Realm Navigation</Badge>
+            </h1>
+            <Realms/>
         </div>
     )
 }
