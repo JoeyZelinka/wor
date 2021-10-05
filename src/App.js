bd1c95701a5b95d7ec92708638408a0cfba81c75
Joey Zelinka <cheflife365@gmail.com>
2021-10-05 16:37:55 -0400

    some routes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,55 @@ import "bootswatch/dist/cyborg/bootstrap.min.css";
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import { motion } from "framer-motion"
-import BlogTemplate from "./components/Blogs/BlogTemplate";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import HomePage from "../src/components/HomePage/HomePage"
+import MtgHome from "../src/components/MtgHome/MtgHome"
+import RPGhome from "../src/components/RPGhome/RPGhome"
 
 // import Realms from "./components/Realms/Realms";
 function App() {
   return (
-    <div className="App">
-      <motion.div
-       animate={{ rotate: 360 }}
-       transition={{ duration: 3 }} 
-      class="header"><img src="pngsmall.png" alt="logo"></img></motion.div>
-      <NavBar/>
-      <BlogTemplate/>
+    <div>
+      <Router>
+        <motion.div
+          animate={{ rotate: 360 }}
+          transition={{ duration: 3 }}
+          class="header"
+          ><img src="pngsmall.png" alt="logo"></img></motion.div>
+        <NavBar />
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/mtghome">
+              <MtgHome />
+            </Route>
+            <Route path="/rpghome">
+              <RPGhome />
+            </Route>
+            {/* <Route path="/worblog">
+              <WorBlog />
+            </Route>
+            <Route path="/whhome">
+              <WHhome />
+            </Route>
+            <Route path="/pokemonhome">
+              <PokemonHome />
+            </Route>
+            <Route path="/fabhome">
+              <FABHome />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route> */}
+          </Switch>
+        </div>
+      </Router>
     </div>
-
   );
 }
 
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Realms from './Realms/Realms.js'
+import Realms from '../Realms/Realms'
 import "bootswatch/dist/cyborg/bootstrap.min.css";
 import { Badge } from 'react-bootstrap'
 
