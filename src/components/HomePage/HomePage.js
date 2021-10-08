0adfe3ea7352a045b4e14d78c7cad964d4a51c3e
Joey Zelinka <cheflife365@gmail.com>
2021-10-08 10:38:55 -0400

    its friday

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,15 +2,40 @@ import React from 'react'
 import Realms from '../Realms/Realms'
 import "bootswatch/dist/cyborg/bootstrap.min.css";
 import { Badge } from 'react-bootstrap'
+import { motion } from 'framer-motion';
 
 
 export default function HomePage() {
-    return (
-        <div>
-            <h1 className="head">
-            <Badge bg="primary">Realm Navigation</Badge>
-            </h1>
-            <Realms/>
+    
+    const variants = {
+        hidden: {
+            opacity: 0,
+            x: 0,
+            y: 500,
+        },
+        show: {
+            opacity: 1,
+            x: 0,
+            y: 0,
+            transition: {
+                duration: 3,
+                
+            }
+        }
+        
+    }
+        return (
+            <div>
+            
+        
+            <motion.h1
+            variants={variants}
+            initial="hidden"
+            animate="show"
+             className="head">
+                <Badge>Welcome to our Realms</Badge>
+            </motion.h1>
+            <Realms />
         </div>
     )
 }
