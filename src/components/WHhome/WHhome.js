b33fe6c535db023fa653ada120b001d778c32962
Joey Zelinka <cheflife365@gmail.com>
2021-10-29 12:04:12 -0400

    almost there

--- src/components/WHhome/WHhome.js
+++ src/components/WHhome/WHhome.js
@@ -57,14 +57,35 @@ export default function WHhome() {
                     <h3 className="multihead">From Around the Multiverse:</h3>
                     <Container className='multicards'>
                     <Card bg="light" style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="40k.png" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
+                    <Card.Img variant="top" src="spikebitspic.jpg" />
+                <Card.Body>
+                        <Card.Title>SpikeyBits:</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                        Counterfeiting Games Workshop is so Hot Right Now
                         </Card.Text>
-                        <Button variant="dark">Go somewhere</Button>
+                        <p>October 29th, 2021</p>
+                        <Button variant="dark" href="https://spikeybits.com/2021/10/counterfeiting-games-workshop-is-so-hot-right-now.html" target="_blank">Head to SpikeyBits!</Button>
+                    </Card.Body>
+                </Card>
+                <Card bg="light" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="sisters.jpg" />
+                <Card.Body>
+                        <Card.Title>Bell of Lost Souls:</Card.Title>
+                        <Card.Text>
+                        Warhammer 40K: A Closer Look at Sororitas Novitiates 
+                        </Card.Text>
+                        <p>October 29th, 2021</p>
+                        <Button variant="dark" href="https://www.belloflostsouls.net/2021/10/warhammer-40k-a-closer-look-at-sororitas-novitiates.html" target="_blank">Head to BoLS!</Button>
+                    </Card.Body>
+                </Card>
+                    <Card bg="light" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="heath.jpg" />
+                    <Card.Body>
+                        <Card.Title>Frontline Gaming:</Card.Title>
+                        <Card.Text>
+                        This Week in the ITC: Orks Win So Cal Open, Calls for Nerf Immediate.
+                        </Card.Text>
+                        <Button variant="dark" href="https://www.frontlinegaming.org/2021/10/27/this-week-in-the-itc-orks-win-so-cal-open-calls-for-nerf-immediate/?fbclid=IwAR3hujYqkUA1hhZPt9kixIkaceWjpoik_w8akWpCgx7W2tIi8oM7Z29GjfM" target="_blank">Head to Frontline!</Button>
                     </Card.Body>
                 </Card>
                 
