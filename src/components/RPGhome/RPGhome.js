1993e3e1cd10b7e203c0d4ea17fb5d688c1db413
Joey Zelinka <cheflife365@gmail.com>
2021-10-31 16:02:38 -0400

    here we go

--- src/components/RPGhome/RPGhome.js
+++ src/components/RPGhome/RPGhome.js
@@ -79,16 +79,41 @@ export default function RPGhome() {
                     <h3 className="multihead">From Around the Multiverse:</h3>
                     <Container className='multicards'>
                     <Card bg="light" style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="rpgdice.png" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="dark">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                        <Card.Img variant="top" src="gruumsh.jpg" />
+                        <Card.Body>
+                            <Card.Title>Bell of Lost Souls:</Card.Title>
+                            <Card.Text>
+                                D&D: An Adventurer’s Guide to Gruums
+
+                            </Card.Text>
+                            <p>October 31st, 2021</p>
+                            <Button variant="dark" href="https://www.belloflostsouls.net/2021/10/dd-an-adventurers-guide-to-gruumsh.html" target="_blank">Head to BoLS!</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card bg="light" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="dnd_news.png" />
+                        <Card.Body>
+                            <Card.Title>En World:</Card.Title>
+                            <Card.Text>
+                            There’s a TOMB RAIDER RPG Coming!
+
+                            </Card.Text>
+                            <p>October 31st, 2021</p>
+                            <Button variant="dark" href="https://www.enworld.org/threads/there%E2%80%99s-a-tomb-raider-rpg-coming.683637/" target="_blank">Head to En World!</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card bg="light" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="pathfinder.jpg" />
+                        <Card.Body>
+                            <Card.Title>Wargamer:</Card.Title>
+                            <Card.Text>
+                                Pathfinder launches its own version of D&D Beyond
+
+                            </Card.Text>
+                            <p>October 27th, 2021</p>
+                            <Button variant="dark" href="https://www.wargamer.com/pathfinder/nexus-launch-dnd-beyond" target="_blank">Head to Wargamer!</Button>
+                        </Card.Body>
+                    </Card>
                 
                 </Container>
                 </div>
